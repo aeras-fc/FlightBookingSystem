b137d688635fe832995ba928a5a48bf24d6ed13f
aeras.alvi <aeras.alvi@L-8BFHV93.ad.fc.net>
2021-10-01 19:42:04 +0530

    Adding the test file

---
+++ BookFlight/src/main/java/com/example/BookFlight/Test.java
@@ -0,0 +1,12 @@
+package com.example.BookFlight;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("In the main function");
+
+	}
+
+}
